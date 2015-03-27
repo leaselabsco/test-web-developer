954515c97ff425faadf8382c34431e70068daa4d
Edrick Clark <edrickclark@gmail.com>
2015-03-26 17:29:47 -0700

    EC Fix Unix/JS DateTime Issue

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var comments = [
-	{'name":"Vito Corleone","comment":'I'm gonna make him an offer he can't refuse.',"timestamp":69379200},
-	{'name":"Terry Benedict","comment":"Congratulations... You're a dead man!","timestamp":1007683200},
-	{'name":"Jessica Rabbit","comment":"I'm not bad. I'm just drawn that way.","timestamp":583113600},
-	{'name":"Martin Brody","comment":"You're gonna need a bigger boat.","timestamp":172281600},
-	{'name":"Jack Dawson","comment":"I'm the king of the world!","timestamp":882489600},
-	{'name":"The Terminator","comment":"Hasta la vista, baby.","timestamp":678326400},
-	{'name":"The Joker","comment":"Why so serious..?","timestamp":1215993600"}
+	{'name":"Vito Corleone","comment":'I'm gonna make him an offer he can't refuse.',"timestamp":69379200000},
+	{'name":"Terry Benedict","comment":"Congratulations... You're a dead man!","timestamp":1007683200000},
+	{'name":"Jessica Rabbit","comment":"I'm not bad. I'm just drawn that way.","timestamp":583113600000},
+	{'name":"Martin Brody","comment":"You're gonna need a bigger boat.","timestamp":172281600000},
+	{'name":"Jack Dawson","comment":"I'm the king of the world!","timestamp":882489600000},
+	{'name":"The Terminator","comment":"Hasta la vista, baby.","timestamp":678326400000},
+	{'name":"The Joker","comment":"Why so serious..?","timestamp":1215993600000"}
 ];
